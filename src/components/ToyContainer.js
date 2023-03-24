bf3297e85c1625e511eec2ce975995d83b71d049
Jahdon Ervin <Jahdonervin@gmail.com>
2023-03-24 19:07:47 -0400

    commit tree 1724a3a279f3a27c37ddc8b9809423e835b6e062

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, onUpdateToy, onDeleteToy}) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-  );
+    <div id="toy-collection">
+    {toys.map(toy => (
+      <ToyCard 
+        key={toy.id}
+        name={toy.name}
+        image={toy.image}
+        likes={toy.likes}
+        toy={toy}
+        onUpdateToy={onUpdateToy}
+        onDeleteToy={onDeleteToy}
+
+      />
+    ))}
+
+  </div>
+);
 }
 
 export default ToyContainer;
